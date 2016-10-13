f440514f1d652a6ac0e8f159108712e619c1b80b
gcz3 <gcz3@pitt.edu>
2016-10-13 10:54:48 -0400

    Style Guide JS Changes
    Looked over the style guide to make sure the JavaScript was following
    the rules

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,14 @@
+/**
+ * errorMessage() returns a new element
+ * runs if user forgot to input something on a required field
+ * based on the passed in message name
+ *
+ * @param {String} message
+ * @return {Element} error
+ */
 function errorMessage(message) {
   var error = document.createElement("p");
-  error.innerHTML = "Error: " + message;
+  error.innerHTML = 'Error: ' + message;
   $(error).css("color", "red");
   $(error).css("font-size", "16px");
   $(error).css("font-weight", "bold");
@@ -8,6 +16,14 @@ function errorMessage(message) {
   return error;
 }
 
+/**
+ * successMessage() returns a new element
+ * runs if user submits form correctly
+ * based on the passed in message name
+ *
+ * @param {String} message
+ * @return {Element} success
+ */
 function successMessage(message) {
   var success = document.createElement("p");
   success.innerHTML = message;
@@ -21,22 +37,22 @@ function successMessage(message) {
 $(document).ready(function() {
   $("#contactForm").submit(function() {
     var name = $("#name").val();
-    if(name == '') {
+    if(name === '') {
       $("#nameField").addClass("has-error")
-      var error = errorMessage("Please enter a name.")
+      var error = errorMessage('Please enter a name.')
       $("#nameField").after(error);
       return false;
     }
     var message = $("#message").val();
-    if(message == '') {
+    if(message === '') {
       $("#messageField").addClass("has-error")
-      var error = errorMessage("Please enter a message.")
+      var error = errorMessage('Please enter a message.')
       $("#messageField").after(error);
       return false;
     }
     $("p").remove();
     $("#contactForm").remove();
-    var success = successMessage("Once PHP and MySQL are used, this will send over the data to a database.");
+    var success = successMessage('Once PHP and MySQL are used, this will send over the data to a database.');
     $("body").append(success);
     return false;
   });
